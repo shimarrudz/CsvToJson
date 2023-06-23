f4071bffefe2fb55ca002c0d5f1044a90f98c9f1
shimarrudz <you@example.com>
2023-06-22 23:10:23 -0300

    fixing: frontend reviwed!

--- csv-to-json-app/src/App.tsx
+++ csv-to-json-app/src/App.tsx
@@ -2,10 +2,11 @@ import React, { useState } from 'react';
 import FileInput from './components/FileInput';
 import ConvertButton from './components/ConvertButton';
 import { convertCSVToJSON, downloadJSONFile } from './utils';
-import './App.css'
+import './App.css';
 
 function App() {
   const [selectedFiles, setSelectedFiles] = useState<FileList | null>(null);
+  const [conversionErrors, setConversionErrors] = useState<string[]>([]);
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const files = event.target.files;
@@ -14,13 +15,20 @@ function App() {
 
   const handleConvertClick = () => {
     if (selectedFiles) {
+      const errors: string[] = [];
+
       Array.from(selectedFiles).forEach((file) => {
         convertCSVToJSON(file)
           .then((jsonData) => {
             downloadJSONFile(jsonData, file.name);
           })
           .catch((error) => {
-            console.error('Error converting file:', file.name, error);
+            const errorMessage = `Error converting file: ${file.name} - ${error.message}`;
+            errors.push(errorMessage);
+            console.error(errorMessage);
+          })
+          .finally(() => {
+            setConversionErrors(errors);
           });
       });
     }
@@ -29,6 +37,13 @@ function App() {
   return (
     <div>
       <h1>CSV to JSON Converter</h1>
+      {conversionErrors.length > 0 && (
+        <div className="error-messages">
+          {conversionErrors.map((error, index) => (
+            <p key={index}>{error}</p>
+          ))}
+        </div>
+      )}
       <FileInput onChange={handleFileChange} />
       <ConvertButton onClick={handleConvertClick} disabled={!selectedFiles} />
     </div>
